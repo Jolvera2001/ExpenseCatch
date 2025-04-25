5bf9b13849bfab13698bf6d292afcbda218d95cd
Jolvera2001 <jolvera6@stedwards.edu>
2025-04-25 18:05:20 -0500

    project init

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Radzen;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ExpenseCatch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var services = new ServiceCollection();
+
+            // web view stuff
+            services.AddWpfBlazorWebView();
+
+            // ui
+            services.AddRadzenComponents();
+            
+            // setup di
+            // services.AddSingleton<>();
+            // ...
+            
+            Resources.Add("mainServices", services.BuildServiceProvider());
+        }
+    }
+
+}
